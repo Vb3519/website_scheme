db4dd892cc08177c953aa6e4d92fb57953e44aed
vb3519 <vb415@bk.ru>
2025-07-02 23:31:50 +0300

    переиспользуемая карточка товара

--- src/pages/catalog/CatalogGroupOnePage.tsx
+++ src/pages/catalog/CatalogGroupOnePage.tsx
@@ -1,5 +1,9 @@
+import { RiFileChartLine } from 'react-icons/ri';
+import { PiStarBold } from 'react-icons/pi';
+
 // Ui:
 import CustomSection from '../../shared/ui/CustomSection';
+import ProductCard from '../../shared/ui/ProductCard';
 
 const CatalogGroupOnePage = () => {
   return (
@@ -8,6 +12,24 @@ const CatalogGroupOnePage = () => {
         <h1 className="text-center">
           Это раздел каталога с группой товаров №1
         </h1>
+
+        <ProductCard>
+          <div className="pb-2 border-b-2 border-b-gray-200 flex">
+            <RiFileChartLine className="m-auto text-9xl text-gray-400/50" />
+            <PiStarBold className="text-3xl text-gray-400/50 cursor-pointer" />
+          </div>
+
+          <p>
+            <span className="font-semibold">Производитель:</span> Пример
+            производителя
+          </p>
+          <p>
+            <span className="font-semibold">Название:</span> Пример названия
+          </p>
+          <p>
+            <span className="font-semibold">Цена:</span> 1000 Руб.
+          </p>
+        </ProductCard>
       </CustomSection>
     </main>
   );
---
+++ src/shared/ui/ProductCard.tsx
@@ -0,0 +1,23 @@
+type ProductCard_Props = React.LiHTMLAttributes<HTMLLIElement> & {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+const ProductCard: React.FC<ProductCard_Props> = ({
+  className,
+  children,
+  ...props
+}: ProductCard_Props) => {
+  return (
+    <li
+      className={`p-2 flex flex-col gap-2 border-2 border-gray-300 rounded-2xl ${
+        className ? className : ''
+      }`}
+      {...props}
+    >
+      {children}
+    </li>
+  );
+};
+
+export default ProductCard;
